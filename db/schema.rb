50f77a01fe02af39c537101eb53e9da146697c28
Djira <djcoully7@gmail.com>
2023-07-07 02:34:01 +0000

    step5

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_17_014042) do
+ActiveRecord::Schema.define(version: 2023_07_01_005519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_labels_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -37,5 +45,6 @@ ActiveRecord::Schema.define(version: 2023_06_17_014042) do
     t.index ["email"], name: "unique_emails", unique: true
   end
 
+  add_foreign_key "labels", "users"
   add_foreign_key "tasks", "users"
 end
